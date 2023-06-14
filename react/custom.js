1139aa1c5278daaa8927df5923b86b1a88c7294e
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-06-15 03:42:07 +0530

    custom hook

---
+++ react/custom.js
@@ -0,0 +1,21 @@
+
+
+
+export default useCounter=(initialValue)=>{
+    const [ state,setState]=useState(initialValue);
+
+    const increment=()=>{
+           setState(state+1)
+    }
+
+    const decrement=()=>{
+        setState(state-1)
+
+    }
+
+    return{
+        state,
+        increment,
+        decrement
+    }
+}
